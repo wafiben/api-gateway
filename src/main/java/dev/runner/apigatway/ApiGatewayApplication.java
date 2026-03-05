8709dffa126eee7bf8d7eb2ca9605c1cd854289c
wafiperso <wafibenjeddou@gmail.com>
2026-03-05 20:45:12 +0100

    fix pie

--- src/main/java/dev/runner/apigatway/ApiGatewayApplication.java
+++ src/main/java/dev/runner/apigatway/ApiGatewayApplication.java
@@ -2,12 +2,16 @@ package dev.runner.apigatway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @SpringBootApplication
 public class ApiGatewayApplication {
 
+    private static final Logger logger = LoggerFactory.getLogger(ApiGatewayApplication.class);
+
     public static void main(String[] args) {
         SpringApplication.run(ApiGatewayApplication.class, args);
-        System.out.println("amplication startted");
+        logger.info("API Gateway started successfully");
     }
 }
